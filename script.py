1fa3bd1f712174750f146bd3ac2c6ba9592438d7
ClemBlue <clement.blin76@gmail.com>
2023-02-23 09:32:50 +0100

    add scheduled popups, wallpaper change and background sound playing

--- script.py
+++ script.py
@@ -1,7 +1,8 @@
 import tkinter as tk
 import random
 import os
-import time
+import schedule
+from playsound import playsound
 
 username = os.getlogin()
 
@@ -18,6 +19,12 @@ creepyText = [
     "It's comming for you " + username
 ]
 
+creepyPic = [
+    "wallpaper1.jpeg",
+    "wallpaper2.jpg",
+    "wallpaper3.jpg"
+]
+
 def randomNumber(min, max):
     randomNB = random.randint(min, max)
     return randomNB
@@ -30,13 +37,36 @@ def popUps():
     root.overrideredirect(True)
 
     nb = randomNumber(0, 9)
-    #root.title('Creepy')
+    width, height = root.winfo_screenwidth(), root.winfo_screenheight()
+    x = randomNumber(0, width)
+    y = randomNumber(0, height)
+    root.geometry('500x50+%d+%d' % (x, y))
 
-    label = tk.Label(root, text=creepyText[nb], bg='black', fg='white')
-    root.geometry('300x100')
+    frame = tk.Frame(root, width=500, height=50, bg='black')
+    frame.grid(row=0, column=0, sticky="NW")
 
-    label.pack()
+    font = ("Ani", 20, "bold")
+
+    label = tk.Label(root, text=creepyText[nb], bg='black', fg='white', font= font)
+    label.place(relx=0.5, rely=0.5, anchor="center")
+
+    #label.pack()
     root.after(1000, root.destroy)
     root.mainloop()
 
-popUps()
+def wallpaper():
+    wallnb = randomNumber(0, 2)
+    os.system("gsettings set org.gnome.desktop.background picture-uri file:///home/"+ username +"/virus/" + creepyPic[wallnb])
+
+def sound():
+    playsound('sound.mp3',0)
+
+
+
+schedule.every(0.05).seconds.do(popUps)
+schedule.every(1).seconds.do(wallpaper)
+schedule.every(1.28).minutes.do(sound)
+
+sound()
+while True:
+    schedule.run_pending()
